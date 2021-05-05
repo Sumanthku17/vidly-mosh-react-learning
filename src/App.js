96bd7ba49652aba1b9f426b818982c2e281ebea7
sumanth <sumanthku17@gmail.com>
2021-05-06 04:11:44 +0530

    making delete button work

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React, { Component } from 'react';
+import Movies from './components/movies';
+
+function App() {
+  return (
+    <main className="container">
+      <Movies/>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/movies.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { getMovies }from '../services/fakeMovieService';
+
+
+class Movies extends Component {
+    state = { 
+        movies : getMovies()
+     };
+
+     handleDelete = (movie) =>
+     {
+        const mov = this.state.movies.filter(m => m._id !== movie._id);
+        this.setState( {movies : mov} )
+     }
+
+    render() { 
+        return <table className="table">
+            <thead>
+                <tr>
+                    <td>Title</td>
+                    <td>Genre</td>
+                    <td>Stock</td>
+                    <td>Rate</td>
+                    <td></td>
+                </tr>
+            </thead>
+
+            <tbody>
+
+                {this.state.movies.map(movie => (
+                <tr key={movie._id}>
+                    <td>{movie.title}</td>
+                    <td>{movie.genre.name}</td>
+                    <td>{movie.numberInStock}</td>
+                    <td>{movie.dailyRentalRate}</td>
+                    <td>
+                        <button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button>
+                    </td>
+                </tr>
+                ))}
+
+                
+            </tbody>
+        </table>
+    }
+}
+ 
+export default Movies;
